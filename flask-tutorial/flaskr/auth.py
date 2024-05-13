9a1377c8ac4d8eab98130424f908c38467de01e1
Alain KABBOUH <alainkabbouh@gmail.com>
2024-05-13 12:14:33 +0200

    Flask flaskr tutorial :
    Added html templates files for authentification and base pages.
    
    Should be working at this stage bet isn't : fixed.
    
    The issues were :
    
    * Templates were not put in the correct folder leading to templates not found erros
    * Errors in templates, missing %...
    * Typo in function call fetchone.
    * Using the wrong command and launching hello instead of flaskr
    
    https://flask.palletsprojects.com/en/3.0.x/tutorial/templates/

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -78,7 +78,7 @@ def load_logged_in_user():
     else:
         g.user = get_db().execute(
             'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fecthone()
+        ).fetchone()
 
 
 @bp.route('/logout')
