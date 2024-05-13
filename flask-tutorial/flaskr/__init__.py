14d8f268cf9a1093237ce5015ee2eeb1ad3c3f29
Alain KABBOUH <alainkabbouh@gmail.com>
2024-05-13 17:49:49 +0200

    Flask flaskr tutorial :
    https://flask.palletsprojects.com/en/3.0.x/tutorial/blog/
    
    * created index.html and blog.py
    
    Blog tutorial chapter not finished.
    Next step is to add the create view in blog.py.

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -13,6 +13,10 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -38,5 +42,6 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
 
     return app
